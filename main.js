6c063ee31f936324cdebd59baaa46c298031b194
Helmut Stormesan <hcstormesan@miuandes.cl>
2024-03-15 16:02:49 -0300

    commit

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtenemos el botón y el div que queremos cambiar
-    var addButton = document.getElementById("addButton");
-    var sectionToChange = document.getElementById("sectionToChange");
-  
-    // Agregamos el listener de evento al botón
-    addButton.addEventListener("click", function() {
-      // Obtenemos el valor del input
-      var inputText = document.getElementById("inputText").value;
-      // Cambiamos el contenido del div al valor del input
-      sectionToChange.textContent = inputText;
-    });
-  });
-  
+function changeSection() {
+  var inputText = document.getElementById("inputText").value;
+  document.getElementById("sectionToChange").textContent = inputText;
+}
