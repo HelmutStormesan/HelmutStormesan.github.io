afd59e7deb8bface8f035b70f5481ea4d31f745c
Helmut Stormesan <hcstormesan@miuandes.cl>
2024-03-14 23:01:47 -0300

    l commit

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-function changeSection() {
-    var inputValue = document.getElementById('inputValue').value;
-    document.getElementById('sectionToChange').innerHTML = inputValue;
+window.onload = function() {
+    // Obtener el valor del almacenamiento local si existe
+    var storedText = localStorage.getItem('inputText');
+    if (storedText) {
+      document.getElementById('sectionToChange').innerText = storedText;
+    }
+  
+    // Función para cambiar la sección con el valor del input
+    function changeSection() {
+      var inputValue = document.getElementById('inputValue').value;
+      document.getElementById('sectionToChange').innerText = inputValue;
+  
+      // Guardar el valor en el almacenamiento local
+      localStorage.setItem('inputText', inputValue);
+    }
+  
+    // Agregar un listener al botón
+    document.getElementById('changeButton').addEventListener('click', changeSection);
   }
+  
