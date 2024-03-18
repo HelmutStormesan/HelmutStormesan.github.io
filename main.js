b789ab9ec6af886d671a0e77466f26b805c31fbc
Helmut Stormesan <hcstormesan@miuandes.cl>
2024-03-18 13:16:23 -0300

    lab02 commit

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ document.getElementById("addButton").addEventListener("click", function() {
   var userInput = document.getElementById("inputText").value;
   document.getElementById("sectionToChange").textContent = userInput;
 });
+
+document.getElementById('toggleView').addEventListener('click', function() {
+  var container = document.querySelector('.container');
+  if (container.style.maxWidth !== '360px') {
+    container.style.maxWidth = '360px';
+    this.textContent = 'Cambiar a Vista Web';
+  } else {
+    container.style.maxWidth = '800px';
+    this.textContent = 'Cambiar a Vista Móvil';
+  }
+});
+
+document.addEventListener('scroll', function() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+});
+
+
